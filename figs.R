b5f4b24ce7f85dd27c5df494693cf3153034fa87
anhoej <jacob@anhoej.net>
2019-03-03 15:22:57 +0100

    Exploring figures.

--- figs.R
+++ figs.R
@@ -1,68 +1,60 @@
 library(tidyverse)
-load('crossrunbox1.Rdata')
+bounds <- readRDS('bounds.rds')
 
-x <- bounds %>% 
-  transmute(n,
-         c_anhoej = ca,
-         l_anhoej = la,
-         c_bestbox = cb,
-         l_bestbox = lb,
-         c_cutbox = ifelse(is.na(cbord), c_bestbox, cbord),
-         l_cutbox = ifelse(is.na(lbord), l_bestbox, lbord),
-         specificity_anhoej = pa.0,
-         specificity_bestbox = pb.0,
-         specificity_cutbox = pc.0,
-         lrposa.1,
-         lrposb.1,
-         lrposc.1,
-         lrposa.2,
-         lrposb.2,
-         lrposc.2,
-         lrnega.1,
-         lrnegb.1,
-         lrnegc.1,
-         lrnega.2,
-         lrnegb.2,
-         lrnegc.2)
-         
-ggplot(x[1:30,], aes(n, lrposa.2)) +
+# Compare limits from anhoej and best box rules
+limits <- bounds %>% 
+  select(n:lb) %>% 
+  gather('key', 'val', -n) %>% 
+  separate(key, c('test', 'rule'), 1) %>% 
+  mutate(test = fct_recode(test, `number of crossings` = 'c',
+                           `longest run` = 'l'),
+         rule = fct_recode(rule, anhoej = 'a',
+                           `best box` = 'b'))
+
+ggplot(limits, aes(n, val, colour = rule)) +
   geom_line() +
-  geom_line(aes(y = lrnega.2)) +
-  scale_y_log10(breaks = c(0.01, 0.1, 0, 10)) +
-  scale_x_continuous(breaks = seq(10, 100, by = 10))
+  facet_wrap(~ test, scale = 'free_y')
 
-ggplot(x[1:30,], aes(n, lrposb.2)) +
+# Wide to tall data
+tall <- bounds %>% 
+  select(-(ca:lbord)) %>%
+  gather('key', 'val', -n) %>% 
+  separate(key, c('test', 'shift'), '_') %>% 
+  mutate(rule = substring(test, nchar(test)),
+         test = substring(test, 1, nchar(test) - 1),
+         shift = as.numeric(shift)) %>% 
+  mutate(rule = fct_recode(rule, anhoej = 'a',
+                           `best box` = 'b',
+                           `cut box` = 'c'))
+
+
+# Plot power function
+ggplot(filter(tall, test == 'p'), aes(n, 1 - val, colour = rule)) +
   geom_line() +
-  geom_line(aes(y = lrnegb.2)) +
-  scale_y_log10(breaks = c(0.01, 0.1, 0, 10))
+  facet_wrap(~ shift) +
+  theme_minimal() +
+  labs(title = 'Power function',
+       y = 'Probability of signal',
+       x = 'N')
 
-ggplot(x[1:30,], aes(n, lrposc.2)) +
+# Plot LR+
+ggplot(filter(tall, test == 'lrpos'), aes(n, val, colour = rule)) +
   geom_line() +
-  geom_line(aes(y = lrnegc.2)) +
-  scale_y_log10(breaks = c(0.01, 0.1, 0, 10))
+  geom_hline(yintercept = 10) +
+  facet_wrap(~ shift) +
+  scale_y_log10() +
+  theme_minimal() +
+  labs(title = 'Positive likelihood ratio',
+       y = 'LR+',
+       x = 'N')
 
-s <- x %>% 
-  select(n, specificity_anhoej:specificity_cutbox) %>% 
-  gather('key', 'val', -n)
-
-ggplot(s, aes(n, val)) +
+# Plot LR-
+ggplot(filter(tall, test == 'lrneg'), aes(n, val, colour = rule)) +
   geom_line() +
-  facet_wrap(~ key)
-
-lrp <- x %>% 
-  select(n, lrposa.2:lrposc.2) %>% 
-  gather('key', 'val', -n)
-
-ggplot(lrp, aes(n, val)) +
-  geom_line() +
-  facet_wrap(~ key) +
-  scale_y_log10(breaks = c(1, 10, 100))
-
-lrn <- x %>% 
-  select(n, lrnega.2:lrnegc.2) %>% 
-  gather('key', 'val', -n)
-
-ggplot(lrn, aes(n, val)) +
-  geom_line() +
-  facet_wrap(~ key) +
-  scale_y_log10(breaks = c(0, 0.1, 0.01))
+  geom_hline(yintercept = 0.1) +
+  facet_wrap(~ shift) +
+  scale_y_log10() +
+  theme_minimal() +
+  labs(title = 'Negative likelihood ratio',
+       y = 'LR-',
+       x = 'N')
